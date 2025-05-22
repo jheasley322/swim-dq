83029a584359c49dff9f7d9a4061ca622dc01807
Joe Easley <jheasley322@gmail.com>
2025-05-22 08:58:07 -0400

    adding swim dq app

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-4">
+      <h1 className="text-3xl font-bold">🏊 SwimDQ is live</h1>
+    </main>
+  );
+}
---
+++ lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5bn-qzFwKQScQW3Xq5SmA717Mt-9J7-g",
+  authDomain: "swimdq-35596.firebaseapp.com",
+  projectId: "swimdq-35596",
+  storageBucket: "swimdq-35596.firebasestorage.app",
+  messagingSenderId: "989686834542",
+  appId: "1:989686834542:web:3146cc7b04c90509c18d7f",
+  measurementId: "G-28GL4F8MHB"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
